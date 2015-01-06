33e4e7b10b941389d7a7071f4e1eab6778af09b1
jolakammu <jolakammu@yahoo.com>
2015-01-06 12:27:14 -0600

    initial commit for test project two

---
+++ src/edu/austincc/main/App.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.austincc.main;
+
+/**
+ * @author jolak_000
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow World");
+
+	}
+
+}
